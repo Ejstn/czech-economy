d251f841315d7c768ae2bcae2f2250f075e7a1dd
Martin Soukup <>
2020-01-13 13:58:22 +0100

    Add empty project from initializr

---
+++ src/main/kotlin/com/estn/economy/EconomyApplication.kt
@@ -0,0 +1,11 @@
+package com.estn.economy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EconomyApplication
+
+fun main(args: Array<String>) {
+	runApplication<EconomyApplication>(*args)
+}

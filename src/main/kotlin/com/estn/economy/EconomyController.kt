fc2c3e5057658bb8933c524dcfb0e68d0b33620a
Ceton <>
2020-01-14 10:51:47 +0100

    Expose current exchange rates with rest controller

---
+++ src/main/kotlin/com/estn/economy/EconomyController.kt
@@ -0,0 +1,7 @@
+package com.estn.economy
+
+/**
+ * Written by Martin Soukup on 14.1.2020.
+ */
+class EconomyController {
+}
---
+++ src/main/kotlin/com/estn/economy/exchangerate/domain/ExchangeRate.kt
@@ -0,0 +1,22 @@
+package com.estn.economy.exchangerate.database
+
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+/**
+ * Written by estn on 13.01.2020.
+ */
+@Entity(name = "echange_rate")
+data class ExchangeRateEntity(
+        @Id @GeneratedValue
+        var id: Int = 0,
+        @Column(name = "date") var date: Date = Date(),
+        @Column(name = "currency_code") var currencyCode: String = "",
+        @Column(name = "currency_name") var currencyName: String = "",
+        @Column(name = "amount") var amount: Int = 0,
+        @Column(name = "exchange_rate") var exchangeRate: Double = 0.0,
+        @Column(name = "country") var country: String = ""
+)

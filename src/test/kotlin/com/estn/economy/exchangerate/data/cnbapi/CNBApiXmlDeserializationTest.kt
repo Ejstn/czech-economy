df87c2e129920af9821ead6ac0f3bda8bcd08db2
Ceton <>
2020-01-27 11:52:43 +0100

    Add test profiles for integration tests - to use test database

--- src/test/kotlin/com/estn/economy/exchangerate/data/cnbapi/CNBApiXmlDeserializationTest.kt
+++ src/test/kotlin/com/estn/economy/exchangerate/data/cnbapi/CNBApiXmlDeserializationTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest
  */
 @Suppress("MemberVisibilityCanBePrivate")
 @SpringBootTest
+@ActiveProfiles("test")
 class CNBApiXmlDeserializationTest {
 
     val input: String =

42172f3f434183ae04f41d8255187a97571d003e
ceton <a@a.com>
2020-05-07 14:39:37 +0200

    Fetch gdp by year from db

--- src/test/kotlin/com/estn/economy/grossdomesticproduct/data/database/GrossDomesticProductRepositoryTest.kt
+++ src/test/kotlin/com/estn/economy/grossdomesticproduct/data/database/GrossDomesticProductRepositoryTest.kt
@@ -1,11 +1,11 @@
 package com.estn.economy.grossdomesticproduct.data.database
 
+import com.estn.economy.grossdomesticproduct.domain.GrossDomesticProductPerYear
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.ActiveProfiles
 
 /**

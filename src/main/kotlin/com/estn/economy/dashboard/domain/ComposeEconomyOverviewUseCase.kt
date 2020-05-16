f01f38d102a66a284c7ae14f05e0bc57a51f66e4
Ceton <a@a.com>
2020-05-16 12:19:48 +0200

    Add inflation

--- src/main/kotlin/com/estn/economy/dashboard/domain/ComposeEconomyOverviewUseCase.kt
+++ src/main/kotlin/com/estn/economy/dashboard/domain/ComposeEconomyOverviewUseCase.kt
@@ -3,22 +3,27 @@ package com.estn.economy.dashboard.domain
 import com.estn.economy.exchangerate.data.database.ExchangeRateRepository
 import com.estn.economy.exchangerate.data.database.toDomain
 import com.estn.economy.exchangerate.domain.ExchangeRate
+import com.estn.economy.inflation.data.InflationRateEntity
+import com.estn.economy.inflation.data.InflationRateRepository
+import com.estn.economy.inflation.data.InflationType
 import org.springframework.stereotype.Service
 
 @Service
 class ComposeEconomyOverviewUseCase(private val exchangeRepository: ExchangeRateRepository,
+                                    private val inflationRepository: InflationRateRepository,
         private val configuration: EconomyOverviewConfiguration) {
 
     fun execute() : EconomyOverview {
 
         val rates = exchangeRepository.findAllRatesFromLastDayWhereCodeLike(configuration.exchangeRates)
                 .map { it.toDomain() }
+        val inflation = inflationRepository.findFirstByTypeOrderByYearDescMonthDesc(InflationType.THIS_MONTH_VS_PREVIOUS_YEARS_MONTH)
 
-        return EconomyOverview(rates)
+        return EconomyOverview(rates, inflation)
     }
 
 
 }
 
-data class EconomyOverview (val exchangeRates: Collection<ExchangeRate>
-                            )
+data class EconomyOverview (val exchangeRates: Collection<ExchangeRate>,
+                            val inflation: InflationRateEntity)

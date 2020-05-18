62f5500215c6ad8955bc68612005fa2c2a7f9f21
Ceton <a@a.com>
2020-05-18 16:48:21 +0200

    Update 4xx and 5xx error templates

--- src/main/kotlin/com/estn/economy/core/presentation/GlobalErrorHandler.kt
+++ src/main/kotlin/com/estn/economy/core/presentation/GlobalErrorHandler.kt
@@ -13,14 +13,18 @@ class GlobalErrorHandler {
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = [NoSuchElementException::class])
     fun handle404(model: Model): String {
-        model["status"] = HttpStatus.NOT_FOUND.value()
+        val status = HttpStatus.NOT_FOUND
+        model["status"] = status.value()
+        model["error"] = status.reasonPhrase
         return "error/4xx"
     }
 
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = [Exception::class])
-    fun handle500(model: Model) : String {
-        model["status"] = HttpStatus.INTERNAL_SERVER_ERROR.value()
+    fun handle500(model: Model): String {
+        val status = HttpStatus.INTERNAL_SERVER_ERROR
+        model["status"] = status.value()
+        model["error"] = status.reasonPhrase
         return "error/5xx"
     }
 

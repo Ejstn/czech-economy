3aa748a509103db0f62c3f29c7224af9b20bc891
Ceton <a@a.com>
2020-05-18 21:22:02 +0200

    Add basic graphs to gdp subpage

--- src/main/kotlin/com/estn/economy/grossdomesticproduct/presentation/GrossDomesticProductController.kt
+++ src/main/kotlin/com/estn/economy/grossdomesticproduct/presentation/GrossDomesticProductController.kt
@@ -1,5 +1,7 @@
 package com.estn.economy.grossdomesticproduct.presentation
 
+import com.estn.economy.grossdomesticproduct.data.database.GrossDomesticProductType
+import com.estn.economy.grossdomesticproduct.domain.FetchGrossDomesticProductUseCase
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/hdp")
-class GrossDomesticProductController {
+class GrossDomesticProductController(private val fetchGdpUseCase: FetchGrossDomesticProductUseCase) {
 
     val template = "pages/gdp"
 
     @GetMapping
-    fun getGdp(model: Model) : String {
+    fun getGdp(model: Model): String {
+        model.addAttribute("nominalGdp", fetchGdpUseCase.fetchGdp(GrossDomesticProductType.NOMINAL))
+        model.addAttribute("realGdp2010Prices", fetchGdpUseCase.fetchGdp(GrossDomesticProductType.REAL_2010_PRICES))
         return template
     }
 

ce5f695f980b40c8ca42a90c9055bfc98a6df145
Ceton <a@a.com>
2020-05-25 16:59:06 +0200

    Fix test

--- src/test/kotlin/com/estn/economy/inflation/presentation/InflationControllerTest.kt
+++ src/test/kotlin/com/estn/economy/inflation/presentation/InflationControllerTest.kt
@@ -4,12 +4,14 @@ import com.estn.economy.core.presentation.Breadcrumbs
 import com.estn.economy.core.presentation.Home
 import com.estn.economy.core.presentation.Inflation
 import com.estn.economy.grossdomesticproduct.presentation.GrossDomesticProductController
+import com.estn.economy.inflation.domain.FetchInflationRateUseCase
 import org.junit.jupiter.api.BeforeEach
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultMatcher
 import org.springframework.test.web.servlet.ResultMatcher.*
@@ -23,6 +25,9 @@ class InflationControllerTest {
 
     @Autowired
     lateinit var mvc: MockMvc
+
+    @MockBean
+    lateinit var fetchInflation: FetchInflationRateUseCase
 
     @BeforeEach
     fun setUp() {

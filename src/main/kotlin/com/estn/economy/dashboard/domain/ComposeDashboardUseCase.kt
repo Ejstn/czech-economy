b6fa300ed65c03b2278166a89443e94d9e8db5da
Ceton <a@a.com>
2020-05-26 17:55:54 +0200

    Remove unused dashboard fields

--- src/main/kotlin/com/estn/economy/dashboard/domain/ComposeDashboardUseCase.kt
+++ src/main/kotlin/com/estn/economy/dashboard/domain/ComposeDashboardUseCase.kt
@@ -1,13 +1,6 @@
 package com.estn.economy.dashboard.domain
 
-import com.estn.economy.nationalbudget.data.BudgetBalanceEntity
-import com.estn.economy.nationalbudget.data.BudgetBalanceRepository
-import com.estn.economy.core.domain.date.DateFormatter
-import com.estn.economy.core.domain.date.translate
 import com.estn.economy.core.presentation.utility.mapToPairs
-import com.estn.economy.exchangerate.domain.ExchangeRate
-import com.estn.economy.exchangerate.domain.FetchExchangeRateUseCase
-import com.estn.economy.grossdomesticproduct.data.database.GrossDomesticProductEntity
 import com.estn.economy.grossdomesticproduct.data.database.GrossDomesticProductType
 import com.estn.economy.grossdomesticproduct.domain.FetchGrossDomesticProductUseCase
 import com.estn.economy.inflation.domain.FetchInflationRateUseCase
@@ -16,20 +9,15 @@ import com.estn.economy.unemploymentrate.domain.FetchUnemploymentRateUseCase
 import org.springframework.stereotype.Service
 
 @Service
-class ComposeDashboardUseCase(private val fetchExchangeRate: FetchExchangeRateUseCase,
-                              private val fetchGdp: FetchGrossDomesticProductUseCase,
+class ComposeDashboardUseCase(private val fetchGdp: FetchGrossDomesticProductUseCase,
                               private val fetchUnemploymentRate: FetchUnemploymentRateUseCase,
                               private val fetchInflation: FetchInflationRateUseCase,
                               private val publicDebtRepository: PublicDebtRepository,
-                              private val composeOverview: ComposeEconomyOverviewUseCase,
-                              private val dateFormatter: DateFormatter) {
+                              private val composeOverview: ComposeEconomyOverviewUseCase) {
 
 
     fun execute(): EconomyDashboard {
-        val exchangeRates = fetchExchangeRate.fetchLatestRates()
-        val date = exchangeRates.first().date
 
-        val formattedDate = " ${date.dayOfWeek.translate(false)} ${dateFormatter.formatDateForFrontEnd(date)}"
         val unemployment = fetchUnemploymentRate.fetchAllUnempRatesAveragedByYear()
         val inflation = fetchInflation.fetchAllYearlyInflationRates()
         val publicDebt = publicDebtRepository.findAll()
@@ -38,8 +26,6 @@ class ComposeDashboardUseCase(private val fetchExchangeRate: FetchExchangeRateUs
 
         return EconomyDashboard(
                 overview = overview,
-                exchangeRatesDate = formattedDate,
-                exchangeRates = exchangeRates,
                 yearlyUnempRates = unemployment.mapToPairs(),
                 yearlyInflationRates = inflation.mapToPairs(),
                 realGdp2010PricesPercentChange = gdp.mapToPairs(),
@@ -49,8 +35,6 @@ class ComposeDashboardUseCase(private val fetchExchangeRate: FetchExchangeRateUs
     }
 
     data class EconomyDashboard(val overview: EconomyOverview,
-                                val exchangeRatesDate: String,
-                                val exchangeRates: Collection<ExchangeRate>,
                                 val realGdp2010PricesPercentChange: List<Pair<Any, Any>>,
                                 val yearlyUnempRates: List<Pair<Any, Any>>,
                                 val yearlyInflationRates: List<Pair<Any, Any>>,

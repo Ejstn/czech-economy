13570c89d1dad4bda42d03f18d77584df420eb78
Ceton <a@a.com>
2020-05-27 13:41:27 +0200

    Add cache test

--- src/test/kotlin/com/estn/economy/core/CacheTest.kt
+++ src/test/kotlin/com/estn/economy/core/CacheTest.kt
@@ -3,15 +3,20 @@ package com.estn.economy.core
 import com.estn.economy.TestProfile
 import com.estn.economy.core.domain.EvictAllCacheUseCase
 import com.estn.economy.core.domain.OutputPercentageData
-import com.estn.economy.dashboard.domain.ComposeDashboardUseCase
+import com.estn.economy.dashboard.domain.*
 import com.estn.economy.grossdomesticproduct.data.database.GrossDomesticProductType
 import com.estn.economy.grossdomesticproduct.domain.FetchGrossDomesticProductUseCase
+import com.estn.economy.inflation.data.InflationRateEntity
+import com.estn.economy.inflation.data.InflationType
+import com.estn.economy.unemploymentrate.data.database.UnemploymentRateEntity
+import com.estn.economy.utility.exampleRate
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import java.time.LocalDate
 
 @SpringBootTest
 @TestProfile
@@ -26,12 +31,29 @@ class CacheTest {
     @MockBean
     lateinit var fetchGdp: FetchGrossDomesticProductUseCase
 
+    @MockBean
+    lateinit var composeOverView: ComposeEconomyOverviewUseCase
+
     @BeforeEach
     fun setUp() {
         evictCache.execute()
 
         given(fetchGdp.fetchPercentChangesPerYear(GrossDomesticProductType.REAL_2010_PRICES))
                 .willReturn(listOf(OutputPercentageData(2015, 5.0), OutputPercentageData(2016, 5.7)))
+        given(composeOverView.execute())
+                .willReturn(EconomyOverview(
+                        exchangeRate = ExchangeRatesOverview(LocalDate.now(), listOf(exampleRate)),
+                        unemployment = UnemploymentRateEntity(
+                                quarter = 1,
+                                year = 2015,
+                                unemploymentRatePercent = 5.0),
+                        latestGdp = LatestGdp(2015, 5.0),
+                        inflation = InflationOverview("leden",
+                                InflationRateEntity(
+                                        month = 1,
+                                        year = 2015,
+                                        type = InflationType.THIS_MONTH_VS_PREVIOUS_MONTH,
+                                        valuePercent = 5.0f))))
     }
 
     @Test

f722fc0862ffbdd9ad3f8929458d304e2142dbcf
Ceton <a@a.com>
2020-06-03 14:47:19 +0200

    Update mvc date format

--- src/main/kotlin/com/estn/economy/salary/domain/SynchronizeSalaryUseCase.kt
+++ src/main/kotlin/com/estn/economy/salary/domain/SynchronizeSalaryUseCase.kt
@@ -3,8 +3,6 @@ package com.estn.economy.salary.domain
 import com.estn.economy.core.data.api.CNBClient
 import com.estn.economy.salary.data.database.SalaryRepository
 import com.estn.economy.salary.data.database.toEntity
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 

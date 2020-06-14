a5f8df007ef02ed4b0416cf9eee481e8f3d83a36
Ceton <a@a.com>
2020-06-14 13:10:41 +0200

    Inject server port for integration test

--- src/test/kotlin/com/estn/economy/EconomyApplicationIntegrationTest.kt
+++ src/test/kotlin/com/estn/economy/EconomyApplicationIntegrationTest.kt
@@ -2,9 +2,11 @@ package com.estn.economy
 
 import com.estn.economy.core.presentation.model.Routing
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.HttpStatus
@@ -20,9 +22,17 @@ class EconomyApplicationIntegrationTest {
 
     lateinit var restTemplate: RestTemplate
 
+    @Value(value = "\${server.port}")
+    var serverPort : Int = 0
+
+    val host = "http://localhost"
+
+    lateinit var baseUrl : String
+
     @BeforeEach
     fun setUp() {
         restTemplate = restTemplateBuilder.build()
+        baseUrl = "$host:$serverPort"
     }
 
     @Test
@@ -41,7 +51,6 @@ class EconomyApplicationIntegrationTest {
     }
 
     private fun `test endpoint returns 200`(endpoint: String) {
-        val baseUrl = "http://localhost:8080/"
         val entity = restTemplate.getForEntity("$baseUrl$endpoint", String::class.java)
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
     }

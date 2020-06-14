d92b43ec2bc7f013dc93b51928319a8de60de757
Ceton <a@a.com>
2020-06-14 14:36:28 +0200

    Another shot

--- src/test/kotlin/com/estn/economy/DisabledInCI.kt
+++ src/test/kotlin/com/estn/economy/DisabledInCI.kt
@@ -2,6 +2,6 @@ package com.estn.economy
 
 import org.springframework.test.context.junit.jupiter.DisabledIf
 
-@DisabledIf(expression = "#{systemProperties['CI'].equal('true')}")
+@DisabledIf(expression = "#{systemProperties['CI'].equals('true')}")
 @Retention(AnnotationRetention.RUNTIME)
 annotation class DisabledInCI

15af4b544ab0b6f6f8ddd6bbc7d9488cb0020dfc
Ceton <a@a.com>
2020-06-14 14:20:52 +0200

    Try to fix disabled in ci

--- src/test/kotlin/com/estn/economy/DisabledInCI.kt
+++ src/test/kotlin/com/estn/economy/DisabledInCI.kt
@@ -3,4 +3,5 @@ package com.estn.economy
 import org.springframework.test.context.junit.jupiter.DisabledIf
 
 @DisabledIf(expression = "#{systemProperties['CI'] == true}")
+@Retention(AnnotationRetention.RUNTIME)
 annotation class DisabledInCI
